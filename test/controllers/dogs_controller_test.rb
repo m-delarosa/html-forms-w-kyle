701171421294cb9eca411230951e55ec04f75304
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-03-17 14:51:14 -0600

    Fix git nesting

---
+++ test/controllers/dogs_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class DogsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get dogs_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get dogs_create_url
+    assert_response :success
+  end
+
+end
